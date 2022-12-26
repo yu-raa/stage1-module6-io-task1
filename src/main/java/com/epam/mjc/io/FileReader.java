1808a408a6f31e7f0bc03adddb935a9eb9e9849c
Yuliya Raikova <yuradev2001@gmail.com>
2022-12-26 16:52:31 +0300

    .

--- src/main/java/com/epam/mjc/io/FileReader.java
+++ src/main/java/com/epam/mjc/io/FileReader.java
@@ -3,23 +3,25 @@ package com.epam.mjc.io;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.util.logging.Logger;
 
 
 public class FileReader {
 
     private String read(File file) {
-        String result = "";
+        StringBuilder result = new StringBuilder();
         try (FileInputStream inputStream = new FileInputStream(file)) {
             int bits;
             while ((bits = inputStream.read()) != -1) {
-                result += (char)bits;
+                result.append((char)bits);
             }
         }
         catch (IOException e) {
-            System.err.println(e.getMessage());
+            Logger logger = Logger.getGlobal();
+            logger.info(e.getMessage());
         }
 
-        return result;
+        return result.toString();
     }
 
     public Profile getDataFromFile(File file) {
@@ -39,7 +41,8 @@ public class FileReader {
             return new Profile(info[0], Integer.parseInt(info[1]), info[2], Long.parseLong(info[3]));
         }
         catch (NumberFormatException e) {
-            System.err.println(e.getMessage());
+            Logger logger = Logger.getGlobal();
+            logger.info(e.getMessage());
             return null;
         }
     }
